a20c0283b4ff94ec61823ea3d7c21001d62ab5a9
Fezilyaa <91498409+Fezilyaa@users.noreply.github.com>
2022-07-01 19:47:52 +0500

    commit three

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -20,7 +20,7 @@ public class User implements UserDetails {
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
     @Column(name = "USER_AGE")
     private int userAge;
